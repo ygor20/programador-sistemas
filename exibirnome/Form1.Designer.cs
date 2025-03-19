24446456e2abce3f580b87401d83603e586e217a
ygor20 <ygor.aoliveira@senacsp.edu.br>
2025-03-19 17:17:09 -0300

    Exibir Nome

---
+++ exibirnome/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace exibirnome
+{
+    partial class frmExibirNome
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtExibirNome = new System.Windows.Forms.TextBox();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtExibirNome
+            // 
+            this.txtExibirNome.Location = new System.Drawing.Point(115, 77);
+            this.txtExibirNome.Name = "txtExibirNome";
+            this.txtExibirNome.Size = new System.Drawing.Size(205, 20);
+            this.txtExibirNome.TabIndex = 0;
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExibir.Location = new System.Drawing.Point(155, 125);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(122, 41);
+            this.btnExibir.TabIndex = 1;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // frmExibirNome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 215);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.txtExibirNome);
+            this.Name = "frmExibirNome";
+            this.Text = "Exibir Nome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtExibirNome;
+        private System.Windows.Forms.Button btnExibir;
+    }
+}
+
---
+++ exibirnome/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exibirnome
+{
+    public partial class frmExibirNome : Form
+    {
+        public frmExibirNome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            string nome = txtExibirNome.Text;
+            MessageBox.Show("Meu nome é:" + "\n" + nome);
+        }
+    }
+}
