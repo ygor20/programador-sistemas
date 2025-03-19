f80eb865b4ecd3794f277bc82b8ee8f83fb84b69
ygor20 <ygor.aoliveira@senacsp.edu.br>
2025-03-19 17:15:54 -0300

    Calculadora de Idade

---
+++ calculodeidade/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculodeidade
+{
+    public partial class frmCalculoIdade : Form
+    {
+        public frmCalculoIdade()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int anonascimento, anoatual, idade; 
+            anonascimento = int.Parse(txtAnoNasc.Text);
+            anoatual = int.Parse(txtAnoAtual.Text);
+            idade = anoatual - anonascimento;
+
+            lblIdade.Text = idade.ToString();
+        }
+    }
+}
