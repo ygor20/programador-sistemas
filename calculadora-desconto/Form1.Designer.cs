0cdfb6dbf1131e0086ad4872abe4217ae60721b6
ygor20 <ygor.aoliveira@senacsp.edu.br>
2025-03-21 14:53:17 -0300

    Sistema de Cálculo de Descontos

---
+++ calculadora-desconto/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace calculadora_desconto
+{
+    partial class frmCalculadoraDesconto
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblValorCompra = new System.Windows.Forms.Label();
+            this.lblInserirDesconto = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtValorCompra = new System.Windows.Forms.TextBox();
+            this.txtDesconto = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblValorFinal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblValorCompra
+            // 
+            this.lblValorCompra.AutoSize = true;
+            this.lblValorCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorCompra.Location = new System.Drawing.Point(57, 141);
+            this.lblValorCompra.Name = "lblValorCompra";
+            this.lblValorCompra.Size = new System.Drawing.Size(240, 24);
+            this.lblValorCompra.TabIndex = 0;
+            this.lblValorCompra.Text = "Insira o valor da compra:";
+            // 
+            // lblInserirDesconto
+            // 
+            this.lblInserirDesconto.AutoSize = true;
+            this.lblInserirDesconto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInserirDesconto.Location = new System.Drawing.Point(57, 193);
+            this.lblInserirDesconto.Name = "lblInserirDesconto";
+            this.lblInserirDesconto.Size = new System.Drawing.Size(491, 24);
+            this.lblInserirDesconto.TabIndex = 1;
+            this.lblInserirDesconto.Text = "Insira o desconto (considerar que 10 significa 10%):";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(57, 318);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(352, 24);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Resultado da compra com desconto:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(205, 27);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(388, 24);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "SISTEMA DE CÁLCULO DE DESCONTO";
+            // 
+            // txtValorCompra
+            // 
+            this.txtValorCompra.Location = new System.Drawing.Point(303, 145);
+            this.txtValorCompra.Name = "txtValorCompra";
+            this.txtValorCompra.Size = new System.Drawing.Size(100, 20);
+            this.txtValorCompra.TabIndex = 4;
+            // 
+            // txtDesconto
+            // 
+            this.txtDesconto.Location = new System.Drawing.Point(554, 193);
+            this.txtDesconto.Name = "txtDesconto";
+            this.txtDesconto.Size = new System.Drawing.Size(100, 20);
+            this.txtDesconto.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkRed;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(660, 187);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(89, 30);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblValorFinal
+            // 
+            this.lblValorFinal.AutoSize = true;
+            this.lblValorFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorFinal.Location = new System.Drawing.Point(415, 318);
+            this.lblValorFinal.Name = "lblValorFinal";
+            this.lblValorFinal.Size = new System.Drawing.Size(0, 24);
+            this.lblValorFinal.TabIndex = 8;
+            // 
+            // frmCalculadoraDesconto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(775, 372);
+            this.Controls.Add(this.lblValorFinal);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDesconto);
+            this.Controls.Add(this.txtValorCompra);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblInserirDesconto);
+            this.Controls.Add(this.lblValorCompra);
+            this.Name = "frmCalculadoraDesconto";
+            this.Text = "Sistema de Cálculo de desconto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValorCompra;
+        private System.Windows.Forms.Label lblInserirDesconto;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtValorCompra;
+        private System.Windows.Forms.TextBox txtDesconto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblValorFinal;
+    }
+}
+
---
+++ calculadora-desconto/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora_desconto
+{
+    public partial class frmCalculadoraDesconto : Form
+    {
+        public frmCalculadoraDesconto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Recebendo o valor da compra e o valor do desconto requirido
+                decimal preco = decimal.Parse(txtValorCompra.Text);
+                decimal desconto = decimal.Parse(txtDesconto.Text);
+
+                // Calculando o valor da compra com o desconto
+                decimal valorDesconto = (preco * desconto) / 100;
+                decimal precoFinal = preco - valorDesconto;
+
+                // Exibindo o valor final
+                lblValorFinal.Text = $"R$ {precoFinal:F2}";
+
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, insira um valor válido para o preço/desconto.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
