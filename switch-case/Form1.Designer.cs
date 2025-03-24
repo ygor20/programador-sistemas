242e21ef1f91f1eca59b5d4d35d18850922f79b2
ygor20 <ygor.aoliveira@senacsp.edu.br>
2025-03-24 17:32:19 -0300

    Switch Case

---
+++ switch-case/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace switch_case
+{
+    partial class frmSwitch
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtCodigoInserido = new System.Windows.Forms.TextBox();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(283, 92);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(253, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Insira um código de 1 a 6";
+            // 
+            // txtCodigoInserido
+            // 
+            this.txtCodigoInserido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoInserido.Location = new System.Drawing.Point(360, 141);
+            this.txtCodigoInserido.Name = "txtCodigoInserido";
+            this.txtCodigoInserido.Size = new System.Drawing.Size(100, 26);
+            this.txtCodigoInserido.TabIndex = 1;
+            this.txtCodigoInserido.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDescricao.Location = new System.Drawing.Point(360, 199);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(100, 23);
+            this.lblDescricao.TabIndex = 2;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.Black;
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.ForeColor = System.Drawing.Color.Yellow;
+            this.btnEnviar.Location = new System.Drawing.Point(349, 261);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(111, 68);
+            this.btnEnviar.TabIndex = 3;
+            this.btnEnviar.Text = "Enviar Código";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // frmSwitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.txtCodigoInserido);
+            this.Controls.Add(this.lblTitulo);
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.Name = "frmSwitch";
+            this.Text = "Condicional Switch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtCodigoInserido;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ switch-case/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace switch_case
+{
+    public partial class frmSwitch : Form
+    {
+        public frmSwitch()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var codigo = Convert.ToInt16(txtCodigoInserido.Text);
+                lblDescricao.Text = GetTipoCombustivel( codigo);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha: " + ex.Message);
+            }
+        }
+        private string GetTipoCombustivel (int codigo)
+        {
+            var result = "";
+            switch (codigo)
+            {
+                case 1:
+                    result = "Gasolina";
+                    break;
+                case 2:
+                    result = "Álcool";
+                    break;
+                case 3:
+                    result = "Flex";
+                    break;
+                case 4:
+                    result = " Gás GNV";
+                    break;
+                case 5:
+                    result = "Diesel";
+                    break;
+                case 6:
+                    result = "Eletricidade";
+                    break;
+                default:
+                    result = "Inválido";
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
